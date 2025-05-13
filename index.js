02d34f7d19be5b69d781772eca8160c053f43011
Blake Godsey <somethingclever182@gmail.com>
2025-05-13 17:09:35 -0500

    first menu and change featured drink

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+
+
+//menu with milkshakes & burgers. menu features for both milkshake and burger
+
+const burgers = ['Hamburger', 'Cheeseburger']
+let featuredDrink = 'Stawberry Milkshake'
+
+
+function addBurger(){
+    const newBurger = "Flatburger"
+    burgers.push(newBurger);
+}
+
+if (true) {
+    let anotherNewBurger = "Maple Bacon Burger"
+    burgers.push(anotherNewBurger);
+}
+addBurger();
+
+//update the featured drink
+function changeFeaturedDrink(){
+
+        featuredDrink= "JavaShake"
+       
+
+}
+changeFeaturedDrink();
+
+
+console.log(burgers);
+console.log(featuredDrink);
